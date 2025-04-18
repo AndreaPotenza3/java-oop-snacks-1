b45215451d0c23827fdec06abed61ea2f7541cd3
AndreaPotenza <andreapoten3@gmail.com>
2025-04-18 12:44:03 +0200

    corretto snack 2

--- src/org/lesson/java/snack2/ContoBancario.java
+++ src/org/lesson/java/snack2/ContoBancario.java
@@ -1,23 +1,39 @@
 package org.lesson.java.snack2;
 
+import java.math.BigDecimal;
 import java.util.Random;
 
 public class ContoBancario {
 
     private int accountNumber; 
-    private Double bankBalance;
+    private BigDecimal bankBalance;
     private String message= "Il saldo del conto numero:";
 
-    public ContoBancario(Double bankBalance) {
+    public ContoBancario() {
         Random rand = new Random();
         this.accountNumber = rand.nextInt(99999);
-        this.bankBalance = null;
+        this.bankBalance = new BigDecimal(0);
     }
 
-    public void setBankBalance(Double bankBalance){
+    private void setBankBalance(BigDecimal bankBalance){
 
         this.bankBalance = bankBalance;
 
+    }
+
+    public void deposit(BigDecimal depositAmount){
+
+        if(depositAmount.compareTo(new BigDecimal(0)) == 1) {
+            this.bankBalance = this.bankBalance.add(depositAmount);
+        }
+    }
+
+    public Boolean retire(BigDecimal retireAmount){
+        if(retireAmount.compareTo(new BigDecimal(0)) == 1 && bankBalance.compareTo(retireAmount) == 1){
+            this.bankBalance = this.bankBalance.subtract(retireAmount);
+            return true;
+        }
+        return false;
     }
 
     public String getBankBalance(){
--- src/org/lesson/java/snack2/Main.java
+++ src/org/lesson/java/snack2/Main.java
@@ -1,12 +1,16 @@
 package org.lesson.java.snack2;
 
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
         
-        ContoBancario conto = new ContoBancario(null); 
+        ContoBancario conto = new ContoBancario(); 
 
-            conto.setBankBalance(1250.99);
+            conto.deposit(new BigDecimal(1250));
+            conto.retire(new BigDecimal(800));
+
 
             System.out.println(conto.getBankBalance());
 
